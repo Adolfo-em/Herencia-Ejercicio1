167ca3eaccf97e9e86d77336f22644f2f29ff475
adolfo-em <Adolfojose.escribano@alu.uclm>
2023-03-09 11:14:20 +0100

    Finalizacion clase abogado

--- src/Abogado.java
+++ src/Abogado.java
@@ -6,5 +6,13 @@ public class Abogado extends  Empleado{
     public double getSalario() {
         return super.getSalario() + 10000;
     }
-
+    public int getHoras() {
+        return super.getHoras() + 5;
+    }
+    public String getMesVacaciones() {
+        return "Julio";
+    }
+    public void mostrarMensaje(){
+        System.out.println("Estoy en el congreso");
+    }
 }
